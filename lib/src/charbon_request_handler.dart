1f9b9497c9722255199f86e173a76ec775e6bd69
Ogromny <ogromnycoding@gmail.com>
2018-08-02 23:55:47 +0200

    Some rewrite

--- lib/src/charbon_request_handler.dart
+++ lib/src/charbon_request_handler.dart
@@ -7,30 +7,29 @@ class CharbonRequestHandler {
   String get path => _path;
   String _path;
 
-  Function _handler;
+  final Function _handler;
 
   CharbonRequestHandler(this._method, this._path, this._handler);
 
   bool matches(CharbonContext charbonContext) {
-    if (charbonContext.request.method == _method) {
-      List<String> pathSegments = _path.split("/");
-      List<String> requestPathSegments =
-          charbonContext.request.uri.path.split("/");
+    if (charbonContext.request.method != _method) return false;
 
-      if (pathSegments.length == requestPathSegments.length) {
-        for (int i = 0; i < pathSegments.length; i++) {
-          String pathSegment = pathSegments[i];
-          String requestPathSegment = requestPathSegments[i];
+    final List<String> pathSegments = _path.split("/");
+    final List<String> requestPathSegments = charbonContext.request.uri.path.split("/");
 
-          if (!_match(pathSegment, requestPathSegment)) {
-            return false;
-          }
-        }
-        return true;
-      }
+    final int pathSegmentsLength = pathSegments.length;
+    final int requestPathSegmentsLength = requestPathSegments.length;
+
+    if (pathSegmentsLength != requestPathSegmentsLength) return false;
+
+    for (int i = 0; i < pathSegmentsLength; i++) {
+      final String pathSegment = pathSegments[i];
+      final String requestPathSegment = requestPathSegments[i];
+
+      if (!_match(pathSegment, requestPathSegment)) return false;
     }
 
-    return false;
+    return true;
   }
 
   bool _match(String pathNode, String requestNode) => pathNode == requestNode;
