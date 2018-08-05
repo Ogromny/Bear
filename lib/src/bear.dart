06a63ec67fbba3093029bf0093b49dd3b2f7822d
Ogromny <ogromnycoding@gmail.com>
2018-08-05 18:18:05 +0200

    Simplified Bear

--- lib/src/bear.dart
+++ lib/src/bear.dart
@@ -1,47 +1,44 @@
 import "dart:io";
 
 import "bear_context.dart";
+import "bear_route.dart";
 import "bear_router.dart";
 
 class Bear {
-  HttpServer _server;
-  BearRouter _router;
+  HttpServer server;
+  final BearRouter router = new BearRouter();
 
-  Bear() : this._router = new BearRouter();
+  Bear();
 
-  void get(String path, Function handler) {
-    _router.add("GET", path, handler);
-  }
+  void get(String path, BearHandler handler) =>
+      router.add("GET", path, handler);
 
-  void post(String path, Function handler) {
-    _router.add("POST", path, handler);
-  }
-  
-  void put(String path, Function handler) {
-    _router.add("PUT", path, handler);
-  }
+  void post(String path, BearHandler handler) =>
+      router.add("POST", path, handler);
 
-  void patch(String path, Function handler) {
-    _router.add("PATCH", path, handler);
-  }
+  void put(String path, BearHandler handler) =>
+      router.add("PUT", path, handler);
 
-  void delete(String path, Function handler) {
-    _router.add("DELETE", path, handler);
-  }
+  void patch(String path, BearHandler handler) =>
+      router.add("PATCH", path, handler);
+
+  void delete(String path, BearHandler handler) =>
+      router.add("DELETE", path, handler);
 
   void listen(InternetAddress host, int port, {bool silent: false}) async {
-    this._server = await HttpServer.bind(host, port);
+    server = await HttpServer.bind(host, port);
 
     if (!silent) print("🐻️ Listening on ${host.address}:${port}");
 
-    await for (HttpRequest request in this._server) {
-      final BearContext bearContext = new BearContext(request);
-      this._router.route(bearContext);
+    await for (HttpRequest request in server) {
+      final context = new BearContext(request);
+
+      router.route(context);
     }
   }
 
   void close({bool force: false, bool silent: false}) {
-    this._server.close(force: force);
+    server.close(force: force);
 
     if (!silent) print("🐻️ Closed");
   }
