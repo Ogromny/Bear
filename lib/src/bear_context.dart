d690be92f3e10630817c7bffe60a9892db9f1503
Ogromny <ogromnycoding@gmail.com>
2018-08-08 00:49:43 +0200

    Added statusCode getter and setter in BearContext

--- lib/src/bear_context.dart
+++ lib/src/bear_context.dart
@@ -3,11 +3,16 @@ import "dart:io";
 class BearContext {
   HttpRequest request;
   HttpResponse response;
+  Map<String, String> params;
 
-  BearContext(this.request) : response = request.response;
+  int get statusCode => response.statusCode;
+  set statusCode(int code) => response.statusCode = code;
+
+  BearContext(this.request)
+      : response = request.response,
+        params = Map.from(request.uri.queryParameters);
 
   void send(String output) => response
     ..write(output)
-    ..done
     ..close();
 }
