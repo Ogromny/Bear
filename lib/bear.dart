fbba3f61b1f7b1cf1ead449571d7fa2f41e52718
Ogromny <ogromnycoding@gmail.com>
2018-08-11 03:19:30 +0200

    Added BearMiddleware{,s} in bear.dart

--- lib/bear.dart
+++ lib/bear.dart
@@ -2,6 +2,8 @@ library bear;
 
 export "src/bear.dart";
 export "src/bear_context.dart";
+export "src/bear_middleware.dart";
+export "src/bear_middlewares.dart";
 export "src/bear_route.dart";
 export "src/bear_router.dart";
 
