bfb1281a71ed60d4ffe54c674aa538be07456c97
Ogromny <ogromnycoding@gmail.com>
2018-08-11 23:40:51 +0200

    Commented BearMiddlewares

--- lib/src/bear_middlewares.dart
+++ lib/src/bear_middlewares.dart
@@ -6,8 +6,11 @@ import "bear_middleware.dart";
 class BearMiddlewares {
   final middlewares = <BearMiddleware>[];
 
+  /// Add a [BearMiddleware] to [middlewares].
   void add(BearMiddleware middleware) => middlewares.add(middleware);
 
+  /// Execute each [BearMiddleware] in [middlewares], then return the
+  /// modified ( or not ) [BearContext].
   Future<BearContext> process(BearContext context) async {
     for (var middleware in middlewares) {
       context = await middleware.process(context);
